ef06b6bbec768b465789e175b36add71d80d6d84
AlekseiSpasiuk <aleksei.spasiuk@yandex.ru>
2021-03-08 01:30:32 +0300

    решено 1 задание 7 урока

---
+++ l7ex1.py
@@ -0,0 +1,25 @@
+class Matrix:
+
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        output = '\n'.join(list(map(str, self.matrix)))
+        return output
+
+    def __add__(self, other):
+        result = []
+        for row in range(len(self.matrix)):
+            row_result = []
+            for column in range(len(self.matrix[row])):
+                row_result.append(self.matrix[row][column]
+                                  + other.matrix[row][column])
+            result.append(row_result)
+        return Matrix(result)
+
+
+a = Matrix([[1, 2], [3, 4], [5, 6]])
+print(a, '\n')
+b = Matrix([[7, 1], [3, 4], [9, 0]])
+print(b, '\n')
+print(a + b)
